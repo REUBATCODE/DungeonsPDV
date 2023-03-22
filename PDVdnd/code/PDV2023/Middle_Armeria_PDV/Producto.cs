fc17124355c033a4150ddd1f9fb19c595e6cd325
Reub Vega <126991341+REUBATCODE@users.noreply.github.com>
2023-03-22 10:50:11 -0700

    Back con tres errores, Middle iniciado, Database creada.
    El back contiene errores que no puedo identificar el por qué.
    Middle iniciado apenas con las clases de producto y el enum de tipo de producto.
    Database creada desde XAMPP es Windows, luego me conecté a MySql desde DBeaver, luego exporté en un archivo de texto la creación de la base de datos y la tabla de productos correctamente.

---
+++ PDVdnd/code/PDV2023/Middle_Armeria_PDV/Producto.cs
@@ -0,0 +1,40 @@
+﻿using Back_CRUDs_BD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Middle_Armeria_PDV
+{
+    public class Producto
+    {
+        //propiedades de la clase
+        public string nombre;
+        public string descripcion;
+        public int precio;
+        public string cod_barras;
+        public string imagen;
+        public Tipo tipo;
+        public int id;
+
+        //vars para reutilizar el crud
+        CRUDs_BD bd;
+
+        public Producto() 
+        {
+            //crear una instancia de MySql a la bd
+            bd = new Back_CRUDs_BD.MySql("localhost", "root", "", "dnd_armeria");
+        }
+        //---MÉTODOS---//
+        public bool crear(string nom, string desc, int precio, string cod_barras, string imagen, Tipo tipo)
+        {
+            List<string> nombresCampos = new List<string>()
+            {
+                "nombre", "descripcion", "precio", "cod_barras", "imagen", "tipo"
+            };
+
+            return this.bd.insertar("equipamiento", )
+        }
+    }
+}
