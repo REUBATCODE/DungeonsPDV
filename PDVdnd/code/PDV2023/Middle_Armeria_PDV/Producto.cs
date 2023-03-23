9eb95b4fa42205dc29c49ee1b5640b91ff574460
Reub Vega <126991341+REUBATCODE@users.noreply.github.com>
2023-03-23 12:39:18 -0700

    Middle actualizado, faltan dos métodos para acabarlo.

--- PDVdnd/code/PDV2023/Middle_Armeria_PDV/Producto.cs
+++ PDVdnd/code/PDV2023/Middle_Armeria_PDV/Producto.cs
@@ -21,7 +21,10 @@ namespace Middle_Armeria_PDV
         //vars para reutilizar el crud
         CRUDs_BD bd;
 
-        public Producto() 
+        //var static de msg de error
+        public static string msgError;
+
+        public Producto()
         {
             //crear una instancia de MySql a la bd
             bd = new Back_CRUDs_BD.MySql("localhost", "root", "", "dnd_armeria");
@@ -33,8 +36,76 @@ namespace Middle_Armeria_PDV
             {
                 "nombre", "descripcion", "precio", "cod_barras", "imagen", "tipo"
             };
+            List<ValoresAInsertar> vals = new List<ValoresAInsertar>();
+            vals.Add(new ValoresAInsertar(nom));
+            vals.Add(new ValoresAInsertar(desc));
+            vals.Add(new ValoresAInsertar(precio.ToString(), false));
+            vals.Add(new ValoresAInsertar(cod_barras));
+            vals.Add(new ValoresAInsertar(imagen));
+            vals.Add(new ValoresAInsertar(tipo.ToString()));
 
-            return this.bd.insertar("equipamiento", )
-        }
+
+            bool resultado = this.bd.insertar("equipamiento", nombresCampos, vals);
+            //validar el resultado
+            if (resultado == false)
+                Producto.msgError = this.bd.msgError;
+            return resultado;
+        }//crear
+
+        public bool modificar(string nom, string desc, int precio, string cod_barras, string imagen, Tipo tipo, int id)
+        {
+            List<string> nombresCampos = new List<string>()
+            {
+                "nombre", "descripcion", "precio", "cod_barras", "imagen", "tipo"
+            };
+            List<ValoresAInsertar> vals = new List<ValoresAInsertar>();
+            vals.Add(new ValoresAInsertar(nom));
+            vals.Add(new ValoresAInsertar(desc));
+            vals.Add(new ValoresAInsertar(precio.ToString(), false));
+            vals.Add(new ValoresAInsertar(cod_barras));
+            vals.Add(new ValoresAInsertar(imagen));
+            vals.Add(new ValoresAInsertar(tipo.ToString()));
+
+
+            bool resultado = this.bd.modificar("equipamiento", nombresCampos, vals, id);
+            //validar el resultado
+            if (resultado == false)
+                Producto.msgError = this.bd.msgError;
+            return resultado;
+        }//modificar
+
+        public bool borrar(int id) 
+        {
+            bool res = this.bd.borrar("equipamiento", id);
+            if(res == false)
+                Producto.msgError = this.bd.msgError;
+            return res; 
+        }//borrar
+
+        public List<object[]> consultarTodos() 
+        {
+            return this.bd.consulta("equipamiento");
+        }//consultar todo
+
+        public int consultarPrecio(int id) 
+        {
+            int precio = 0;
+            List<object[]> res = this.bd.consulta("equipamiento", "id=" + id);
+            //validamos que traiga un elemento la lista
+            if(res.Count==1) 
+            {
+                object[] tempo = res[0];
+                precio = int.Parse(tempo[3].ToString());
+            }
+            else
+            {
+                Producto.msgError=this.bd.msgError;
+                precio = -1;
+            }
+            return precio;
+        }//consultar precio por id
+
+
+
     }
 }
